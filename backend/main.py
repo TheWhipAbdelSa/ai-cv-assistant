5cc5f0163dc185b42cc92a292289710760f15a50
TheWhipAbdelSa <abdel.sadaqi@hotmail.com>
2026-06-11 16:11:23 +0200

    Allow frontend origins in CORS

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "http://localhost:5174",
+        "https://lively-water-023078503.7.azurestaticapps.net",
     ],
     allow_credentials=True,
     allow_methods=["*"],
